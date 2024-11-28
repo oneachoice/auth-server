8afa4816433c974e3aab81d63ddeb4bfb7e905f4
oneachoice <oneachoice@gmail.com>
2024-11-28 17:05:24 +0900

    Refactor: Refactor the joining process

--- src/main/java/oneachoice/auth/controller/JoinController.java
+++ src/main/java/oneachoice/auth/controller/JoinController.java
@@ -7,8 +7,6 @@ import oneachoice.auth.dto.request.JoinDTO;
 import oneachoice.auth.dto.response.MessageDTO;
 import oneachoice.auth.service.JoinService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,21 +19,11 @@ public class JoinController {
     private final JoinService joinService;
 
     @PostMapping("/join")
-    public ResponseEntity<MessageDTO> join(@ModelAttribute @Valid JoinDTO joinDTO) {
+    public ResponseEntity<MessageDTO> join(@Valid @ModelAttribute JoinDTO joinDTO) {
 
         if (joinService.join(joinDTO))
             return ResponseEntity.ok(new MessageDTO("Joining success"));
         else
             return ResponseEntity.badRequest().body(new MessageDTO("Joining failure"));
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<MessageDTO>  handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-
-        log.info("유효하지 않은 회원가입 요청 발생: {}", ex.toString());
-
-        MessageDTO messageDTO = new MessageDTO("유효하지 않은 회원가입 요청입니다.");
-
-        return ResponseEntity.badRequest().body(messageDTO);
     }
 }
--- src/main/java/oneachoice/auth/dto/request/JoinDTO.java
+++ src/main/java/oneachoice/auth/dto/request/JoinDTO.java
@@ -7,9 +7,9 @@ import lombok.Data;
 @Data
 public class JoinDTO {
 
-    @Email
+    @Email(message = "유효하지 않은 이메일입니다.")
     private String email;
     
-    @NotBlank // 비밀번호 정책에 따라 제약사항이 변경돼야함
+    @NotBlank(message = "유요하지 않은 비밀번호입니다.") // 비밀번호 정책에 따라 제약사항이 변경돼야함
     private String password;
 }
